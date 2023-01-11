3a14e69f8102f9abd865c36b30f9db56fcd96a31
Fritz Michael Gschwantner <fmg@inspiredminds.at>
2023-01-11 23:41:34 +0000

    fix type error

--- src/Widget/FormCountrySelect.php
+++ src/Widget/FormCountrySelect.php
@@ -29,7 +29,7 @@ class FormCountrySelect extends FormSelect
         }
 
         // Replace insert tags
-        if (str_contains($this->varValue, '{{')) {
+        if (str_contains($this->varValue ?? '', '{{')) {
             $this->varValue = System::getContainer()->get('contao.insert_tag.parser')->parse($this->varValue);
         }
 
